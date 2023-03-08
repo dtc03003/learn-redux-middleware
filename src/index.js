854102a5ac3b5dd91fbc0dca9e6871f4d9b0046c
김동현 <dtc03003@naver.com>
2023-03-08 22:19:57 +0900

    작업환경 준비

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import rootReducer from './modules';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(rootReducer);
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <Provider store={store}>
     <App />
-);
+  </Provider>
+)
