44d2b5fefe89bb4f0de0cb80aee482a906272103
김동현 <dtc03003@naver.com>
2023-03-09 22:48:54 +0900

    redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import App from './App';
 import rootReducer from './modules';
+// import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger'
+import ReduxThunk from 'redux-thunk'
 
-const store = createStore(rootReducer);
+const logger = createLogger();
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 // ReactDOM.render(
 //   <Provider store={store}>
